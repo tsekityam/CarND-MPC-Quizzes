a69a6a204ae1a9b3776df1ef1e971bb476125061
Tse Kit Yam <me@kytse.com>
2017-06-24 23:16:34 +0800

    Smooth the steering angle

--- mpc_to_line/src/MPC.cpp
+++ mpc_to_line/src/MPC.cpp
@@ -68,7 +68,7 @@ class FG_eval {
       fg[0] += CppAD::pow(vars[delta_start + t], 2);
     }
     for (size_t t = 0; t < N - 2; t++) {
-      fg[0] += CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
+      fg[0] += 500 * CppAD::pow(vars[delta_start + t + 1] - vars[delta_start + t], 2);
       fg[0] += CppAD::pow(vars[a_start + t + 1] - vars[a_start + t], 2);
     }
 
